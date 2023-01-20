97ef5a6931fa76df2c845daee9a084286de60ae7
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-01-20 19:06:53 +0900

    ユーザー新規登録、ログイン、ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :birthday, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
   with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
     validates :last_name
@@ -15,4 +16,8 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
+  validates :birthday, presence: true
+
+  has_many :items
+  has_many :records
 end
