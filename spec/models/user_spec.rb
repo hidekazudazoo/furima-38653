717061568e4e9293d607e6d844b4b5fdca3fff83
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-01-21 21:14:41 +0900

    Rspecの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+end
+
+//# Nickname can't be blank
+//# Email can't be blank
+//# Password can't be blank
+//# Password is invalid
+//# Last name can't be blank
+//# Last name is invalid
+//# First name can't be blank
+//# First name is invalid
+//# Last name kana can't be blank
+//# Last name kana is invalid
+//# First name kana can't be blank
+//# First name kana is invalid
+//# Birthday can't be blank
