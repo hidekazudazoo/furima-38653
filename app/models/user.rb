00cbb6412dd10c126d1409edf05a018d30e599f4
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-01-22 09:11:00 +0900

    ユーザーモデルテストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶ一]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ一]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
   end
-  with_options presence: true, format: {with: /\A[ァ-ヶ一]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday, presence: true
 
   has_many :items
   has_many :records
