0b9ac32c82e79733d235f012ecd6fe8a3f51799b
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-01-23 19:09:26 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,11 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :describe, :category, :condition, :delivery_charge, :prefecture, :days_to_ship, :price).merge(user_id: current_user.id)
+  end
+
 end
