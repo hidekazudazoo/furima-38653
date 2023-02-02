4c47f0000f3ae735770fa19f4f2b05b380f417c9
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-02-02 19:01:31 +0900

    バリデーションの設定、コントローラーの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :record
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id,      numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number
 end
