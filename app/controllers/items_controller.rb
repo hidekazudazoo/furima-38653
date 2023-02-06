cfe5451340f9151bc9555ec9d7fc2146e2f1f4df
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-02-07 07:17:50 +0900

    売却済商品はトップページに遷移する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if @item.user == current_user
-
+    return if @item.user == current_user && @item.order.blank?
     redirect_to root_path
   end
 
