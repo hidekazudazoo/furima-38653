3db47e9a6cb6548cbb86026b8c425cdfb0ca5e59
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-02-06 17:44:28 +0900

    コントローラーとモデルの整理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
@@ -11,13 +12,14 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order_id
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
-    validates :postal_code
-    validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+    validates :postal_code, format: {with: /\A[0-9]{3}[-][0-9]{4}\z/, message: "例）123-4567"}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: {with: /\d{10,11}/}, length: {maximum: 11}
     validates :user_id
     validates :item_id
   end
 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    order = Order.create(item_id: params[:item_id], user_id: current_user.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
